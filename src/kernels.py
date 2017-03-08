a10c745b74dd161e0d79d0f3a5c3ccfafc324e3f
jachdou <juliette.achdou@free.fr>
2017-03-08 11:16:44 +0100

    added wavelet kernel

--- src/kernels.py
+++ src/kernels.py
@@ -69,3 +69,11 @@ def rbf_kernel(X, Y, gamma):
 def laplacian_kernel(X,Y, sigma):
     dists = euclidean_dist(X, Y)
     return np.exp(-1 / sigma * np.sqrt(dists))
+
+def mother_wavelet(X):
+    return np.cos(1.75*X)* np.exp(-X**2)
+
+def simple_wavelet_kernel(X,Y, param = 10):
+    column= mother_wavelet((X-Y)/param )
+    return np.product(column)
+    
